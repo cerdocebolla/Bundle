b18444860796a540bdfbd9c66919c6d05da8ad22
Cerdocebolla's Software <lordofsuperium17@gmail.com>
2025-07-29 10:42:06 +0000

    The PIALLO Software has been added inside Bundle.

---
+++ Bundle/pialloBundle.lua
@@ -0,0 +1,34 @@
+loca PIALLO = softwareSet.force = true 
+else if goDown apply.Swap.true = false 
+    set as SoftwareBundle.force = true then 
+        access software.let = true 
+        then when 
+        else if accesed.SoftwareBundle = createBundle 
+        else if support.True then 
+            askedForPermission 
+            when setTrue.elseIfConnnection = true 
+    direct install.Driver = true 
+    then askedForLight 
+        activated Software.PIALLO = true 
+        when BundleSoftware=noSpace.whenSet 
+    else if interaction = True 
+        then setFalse.let = true.force 
+            when occur.ErrorActivate 
+            print BerrorCode goDown
+            ("BerrorCode = 9432
+            BerrorCode = 9323
+            BerrorCode = 0193
+            BerrorCode = 2387
+            BerrorCode = 9912
+            BerrorCode = 3214
+            BerrorCode = 0008
+            BerrorCode = 9999
+            BerrorCode = 8888
+            BerrorCode = 0234
+            BerrorCode = 3339
+            BerrorCode = 0000
+
+            then endInstant.end 
+        end)
+    end)
+}
